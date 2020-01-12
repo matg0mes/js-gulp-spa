787e55572024546c8a9676621500dfe3208c9877
Matheus Gomes <hotmus8500@gmail.com>
2020-01-12 00:20:06 -0300

    Estruturando builds da aplicação

--- gulpTasks/app.js
+++ gulpTasks/app.js
@@ -1,5 +1,8 @@
 const gulp = require("gulp");
 
-gulp.task("app", ["app.html", "app.css", "app.js", "app.imgs"], () => {
-    
-});
+gulp.task("app", ["app.html", "app.css", "app.js", "app.imgs"]);
+
+gulp.task('app.html', () => {})
+gulp.task('app.css', () => {})
+gulp.task('app.js', () => {})
+gulp.task('app.imgs', () => {})
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const util = require("gulp-util");
+// const sequence = require('run-sequence')
+
+require('./gulpTasks/app')
+require('./gulpTasks/deps')
+require('./gulpTasks/servidor')
+
+// O gulp por startar as tasks em paralelo, ele pode não conseguir start o servidor com as
+// configurações certas, por isso usamo o plugin run-sequence
+gulp.task('default', () =>{
+    if(util.env.production){
+        // sequence('deps', 'app')
+        gulp.start('deps', 'app')
+    } else {
+        // sequence('deps', 'app', 'servidor')
+        gulp.start('deps', 'app', 'servidor')
+    }
+})
